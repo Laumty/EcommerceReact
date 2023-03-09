ac6b1018f938608c1561376d87528676e179e69e
Laumty <Lautmarquez@gmail.com>
2023-03-09 15:39:46 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Footer from "./Components/Footer/Footer";
+import ItemCount from "./Components/ItemCount/ItemCount";
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
 import Navbar from "./Components/Navbar/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+
   return (
     <div>
       <Navbar />
-      <ItemListContainer />
-      <Footer />
+        <ItemListContainer />
+        <Footer />
     </div>
   );
 }
